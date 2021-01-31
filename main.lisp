fdac47e59cc429efea073bad1b805b7c3cd07f32
jcguu95 <jcguu95@gmail.com>
2021-01-31 10:50:42 -0600

    fix minor

--- main.lisp
+++ main.lisp
@@ -1,19 +1,24 @@
 (in-package :curfew)
 
 (setf *url-list*
-  '("www.youtube.com" "news.ycombinator.com"
-    "www.reddit.com" "old.reddit.com"
-    "www.facebook.com" "www.messenger.com" "www.instagram.com"))
+      '(
+        "www.youtube.com" "news.ycombinator.com"
+        "www.reddit.com" "old.reddit.com"
+        "www.facebook.com" "www.messenger.com" "www.instagram.com"
+        ))
 
 (setf *instruction*
-  '(((1730 1700) (mapcar #'lock-url *url-list*))
-    ;; FIXME remember that lock-url has a weird bug.
-    ((1700 1730) (mapcar #'unlock-url *url-list*))
-    ((1700 1900) (slock-all))
-    ((1930 0500) (slock-all))))
+      '(
+        ((1700 1900) (slock-all))
+        ((1930 0500) (slock-all))
+
+        ((1730 1700) (mapcar #'lock-url *url-list*))
+        ((1700 1730) (mapcar #'unlock-url *url-list*))
+        ))
+
+
 
 (defun main ()
   "Main entry point."
-  (mapcar #'within=>do *instruction*))
-
-(main)
+  (mapcar #'within=>do *instruction*)
+  (format t "Function #'main executed."))
