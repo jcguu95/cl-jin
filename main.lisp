1f6814414727486977c5010601fcbe175d369289
jcguu95 <jcguu95@gmail.com>
2021-01-31 10:08:00 -0600

    replace macro with function.. still cant handle macros confidently

--- main.lisp
+++ main.lisp
@@ -10,18 +10,19 @@
   "Main entry point."
   ;; FIXME remember that lock-url has a weird bug.
   (within=>do
-   ((1730 1700) (mapcar #'lock-url *url-list*)))
+   '((1730 1700) (mapcar #'lock-url *url-list*)))
   (within=>do
-   ((1700 1730) (mapcar #'unlock-url *url-list*)))
+   '((1700 1730) (mapcar #'unlock-url *url-list*)))
   (within=>do
-   ((1700 1900) (slock-all)))
+   '((1700 1900) (slock-all)))
   (within=>do
-   ((1930 0500) (slock-all))))
+   '((1930 0500) (slock-all))))
 
-(defmacro within=>do (pair)
-  (let ((interval (nth 0 pair))
-        (action (nth 1 pair)))
-  ;; E.g. (within=>do ((1700 1900) (slock-all)))
-  `(when (funcall (time-within ,@interval)
-                  (now-in-int))
-     ,action)))
+(defun within=>do (pair)
+  (unless (eq (length pair) 2)
+    (error "Input should be a list of length 2."))
+  (let ((interval (car pair))
+        (action (car (cdr pair))))
+    (when (funcall (apply #'time-within interval)
+                   (now-in-int))
+      (eval action))))
