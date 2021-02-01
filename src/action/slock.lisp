f90b6bf1d3e8116e472b6f8221a939944112068f
jin <jcguu95@gmail.com>
2021-01-31 20:13:50 -0600

    fix minor issue: slock has to be called by a user.

--- src/action/slock.lisp
+++ src/action/slock.lisp
@@ -1,15 +1,15 @@
 (in-package :curfew)
 
 ;; slock
-(defun slock (n)
-  "Screen lock the N-th display.
+(defun slock (user n)
+  "Screen lock the N-th display as USER.
 
 _Remark_ Depends on the binary slock."
   (let ((display n))
-    (format t "Applying slock to DISPLAY=:~a" display)
+    (format t "Applying slock to DISPLAY=:~a~%" display)
     (uiop:run-program
-     (format nil "export DISPLAY=:~a ; slock &" display))))
+     (format nil "su - ~a -c \"export DISPLAY=:~a ; slock &\"" user display))))
 
-(defun slock-all ()
+(defun slock-all (user)
   (loop for n in '(0 1 2 3 4 5 6 7 8 9)
-        do (slock n)))
+        do (slock user n)))
