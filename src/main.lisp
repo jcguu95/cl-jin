735979610da0cd625886b721911be8a36694778c
jin <jcguu95@gmail.com>
2021-02-01 18:25:52 -0600

    minor

--- src/main.lisp
+++ src/main.lisp
@@ -12,8 +12,8 @@
         ((1700 1900) (slock-all "jin"))
         ((1930 0500) (slock-all "jin"))
 
-        ((1730 1700) (mapcar #'lock-url *url-list*))
-        ((1700 1730) (mapcar #'unlock-url *url-list*))
+        ((1700 1630) (mapcar #'lock-url *url-list*))
+        ((1630 1700) (mapcar #'unlock-url *url-list*))
         ))
 
 (defun main ()
