387fb69352a6645dda387a4dbdf4849ab75cbaa9
jcguu95 <jcguu95@gmail.com>
2021-02-04 06:41:25 -0600

    untested patch.

--- src/action/kill-app.lisp
+++ src/action/kill-app.lisp
@@ -3,4 +3,4 @@
 ;; TODO untested
 (defun kill-app (app)
   (format t "Killing app ~a.." app)
-  (uiop:run-program '("pkill" app)))
+  (uiop:run-program `("pkill" ,app)))
--- src/action/kill-user.lisp
+++ src/action/kill-user.lisp
@@ -3,9 +3,9 @@
 ;; TODO testing
 (defun kill-user (user)
   (format t "Suspending user ~a.." user)
-  (uiop:run-program '("pkill" "-STOP" "-u" user)))
+  (uiop:run-program `("pkill" "-STOP" "-u" ,user)))
 
 ;; TODO testing
 (defun continue-user (user)
   (format t "Continuing user ~a.." user)
-  (uiop:run-program '("pkill" "-CONT" "-u" user)))
+  (uiop:run-program `("pkill" "-CONT" "-u" ,user)))
