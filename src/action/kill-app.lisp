73df19d7fe42926a0d7c5716c8e825d796883e97
jin <jcguu95@gmail.com>
2021-02-04 06:28:36 -0600

    cannot find bug.. commit and push first

--- src/action/kill-app.lisp
+++ src/action/kill-app.lisp
@@ -1,6 +1,6 @@
 (in-package :curfew)
 
-;; kill app
+;; TODO untested
 (defun kill-app (app)
   (format t "Killing app ~a.." app)
   (uiop:run-program '("pkill" app)))
--- src/action/kill-user.lisp
+++ src/action/kill-user.lisp
@@ -1,9 +1,11 @@
 (in-package :curfew)
 
+;; TODO testing
 (defun kill-user (user)
-  (format t "Killing user ~a.." user)
+  (format t "Suspending user ~a.." user)
   (uiop:run-program '("pkill" "-STOP" "-u" user)))
 
+;; TODO testing
 (defun continue-user (user)
   (format t "Continuing user ~a.." user)
   (uiop:run-program '("pkill" "-CONT" "-u" user)))
