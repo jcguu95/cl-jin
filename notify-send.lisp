8ba426f03f895e2dfc5032a4262195128b47dee6
jcguu95 <jcguu95@gmail.com>
2021-02-08 12:54:44 -0600

    fix notify-send

--- notify-send.lisp
+++ notify-send.lisp
@@ -1,13 +1,28 @@
 (in-package :jin)
 
-(defun notify-send (title content)
-  "Send notification using dmenu."
-  ;; TODO Put this to another personal package.
-  ;; taken from https://github.com/death/dbus/blob/master/examples/notify.lisp
-  ;; TODO Add argument for controlling how long it shows.
-  (dbus:with-open-bus (bus (dbus:session-server-addresses))
-    (dbus:with-introspected-object
-        (notifications bus "/org/freedesktop/Notifications"
-                       "org.freedesktop.Notifications")
-      (notifications "org.freedesktop.Notifications" "Notify"
-                     "Test" 0 "" title content '() '() -1))))
+(defun notify-send (title content &key (expire-time 0))
+  "Example usage: (notify-send \"A\" \"B\" :expire-time 0.2222)."
+  (uiop:run-program
+   (format nil "notify-send \"~a\" \"~a\" -t ~a"
+           title content (ceiling (* 1000 expire-time)))))
+
+;; (notify-send "A" "B" :expire-time 2)
+;; (notify-send "A" "B" :expire-time 0.55)
+
+
+
+;;;; Deprecated cuz I dunno how to change expire time.
+;;
+;; (defun notify-send (title content)
+;;   "Send notification using dmenu."
+;;   ;; TODO Put this to another personal package.
+;;   ;; taken from https://github.com/death/dbus/blob/master/examples/notify.lisp
+;;   ;; TODO Add argument for controlling how long it shows.
+;;   (dbus:with-open-bus (bus (dbus:session-server-addresses))
+;;     (dbus:with-introspected-object
+;;         (notifications bus "/org/freedesktop/Notifications"
+;;                        "org.freedesktop.Notifications")
+;;       (notifications "org.freedesktop.Notifications" "Notify"
+;;                      "Test" 0 "" title content '() '() -1))))
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
