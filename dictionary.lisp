794048d33e485795e8a715f440a47b6bb8f031b2
jcguu95 <jcguu95@gmail.com>
2021-02-08 06:08:39 -0600

    commented the code

--- dictionary.lisp
+++ dictionary.lisp
@@ -25,17 +25,14 @@
 (defun sentence->words (sentence)
   "Break the input SENTENCE into a list of words."
   (remove-duplicates
-   (split-sequence:split-sequence
-    #\Space
-    (string-downcase
-     (lint sentence)))
+   (split-sequence:split-sequence #\Space
+    (string-downcase (lint sentence)))
    :test #'string-equal))
 
 (defun sentence->dmenu (sentence)
   "Break the input SENTENCE into a list of words. Let the user
 choose a word by using dmenu. Return the selected word."
   ;; Usage: set sentence to be (trivial-clipboard:text)
-  "Entry point of this program."
   (uiop:run-program
    (format nil "echo -e \"~a\" | dmenu"
            (format nil "~{~a\\n~}"
@@ -50,6 +47,7 @@ choose a word by using dmenu. Return the selected word."
 (defun lookup-word (word)
   "Look up the input word WORD by using sdcv. Format the output
 as a string."
+  ;; This relies on the personal dictionary collection in *DICT-DIR*.
   (uiop:run-program
    (format nil
            "sdcv --data-dir=\"~a\" -n ~a | sed 's/^[ \t\n ]*//g'"
@@ -60,6 +58,7 @@ as a string."
   "Send notification using dmenu."
   ;; TODO Put this to another personal package.
   ;; taken from https://github.com/death/dbus/blob/master/examples/notify.lisp
+  ;; TODO Add argument for controlling how long it shows.
   (dbus:with-open-bus (bus (dbus:session-server-addresses))
     (dbus:with-introspected-object
         (notifications bus "/org/freedesktop/Notifications"
@@ -74,10 +73,16 @@ using notify-send."
   (let* ((word (if force
                    string
                    (sentence->dmenu string))))
+
+    ;; lookup WORD and push to notification
     (notify "word.lisp" (lookup-word word))
+
+    ;; if selected WORD isn't in the STRING, strip the STRING
     (unless (member word (sentence->words string)
                     :test #'string-equal)
       (setf string ""))
+
+    ;; wrap entry with time and context; render as a string.
     (let ((time (local-time:now))
           (context ""))
       (format nil "(\"~a\"~% \"~a\"~% ~s~% ~s)~%~%"
@@ -90,6 +95,8 @@ dictionary. Announce the result by notify-send. And save the
 result to a clip file."
   (let* ((clip (trivial-clipboard:text))
          (result (lookup-dict-string clip)))
+
+    ;; write result to file
     (with-open-file (file (pathname "~/.nb/clip.txt")
                           :direction :output
                           :if-exists :append
@@ -108,10 +115,17 @@ result to a clip file."
          (word (nth 1 entry))
          (sentence (nth 2 entry))
          (context (nth 3 entry)))
+
+    ;; add word to history and truncate if needed
     (progn
       (push word *review-hist*)
-      (setf *review-hist* (subseq *review-hist* 0 *review-hist-length*)))
+      (setf *review-hist* (subseq *review-hist*
+                                  0 *review-hist-length*)))
+
+    ;; if LOOKUP is set T, lookup the string without word selection.
     (when lookup (lookup-dict-string word :force t))
+
+    ;; format the final result as a string.
     (format nil "~a~%~%~a~%~%[context]~%~a" word sentence context)))
 
 (defun random-review! ()
