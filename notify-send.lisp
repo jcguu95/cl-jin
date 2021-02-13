f002fbb4d2f288501c194ea0c10ce6b772338928
jcguu95 <jcguu95@gmail.com>
2021-02-13 06:44:04 -0600

    notify-send without bashism <3

--- notify-send.lisp
+++ notify-send.lisp
@@ -1,29 +1,24 @@
 (in-package :jin)
 
-(defun notify-send (title content &key (expire-time 0))
-  "Example usage: (notify-send \"A\" \"B\" :expire-time 0.2222)."
-  ;; FIXME This does not take care of dbus carefully.
-  (uiop:run-program
-   (format nil "notify-send \"~a\" \"~a\" -t ~a"
-           title content (ceiling (* 1000 expire-time)))))
-
+;; (defun notify-send (title content &key (expire-time 0))
+;;   "Example usage: (notify-send \"A\" \"B\" :expire-time 0.2222)."
+;;   ;; FIXME This does not take care of dbus carefully.
+;;   ;; FIXME Special characters are hard to escape.. deprecated.
+;;   (uiop:run-program
+;;    (format nil "notify-send \"~a\" \"~a\" -t ~a"
+;;            title content (ceiling (* 1000 expire-time)))))
 ;; (notify-send "A" "B" :expire-time 2)
 ;; (notify-send "A" "B" :expire-time 0.55)
 
-
-
-;;;; Deprecated cuz I dunno how to change expire time.
-;;
-;; (defun notify-send (title content)
-;;   "Send notification using dmenu."
-;;   ;; TODO Put this to another personal package.
-;;   ;; taken from https://github.com/death/dbus/blob/master/examples/notify.lisp
-;;   ;; TODO Add argument for controlling how long it shows.
-;;   (dbus:with-open-bus (bus (dbus:session-server-addresses))
-;;     (dbus:with-introspected-object
-;;         (notifications bus "/org/freedesktop/Notifications"
-;;                        "org.freedesktop.Notifications")
-;;       (notifications "org.freedesktop.Notifications" "Notify"
-;;                      "Test" 0 "" title content '() '() -1))))
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun notify-send (title content &key (expire-timeout 0))
+  "Send notification using dmenu. EXPIRE-TIMEOUT is an INT32 in
+milisecond; 0 means infinite while (-1) means default."
+  ;; taken from https://github.com/death/dbus/blob/master/examples/notify.lisp
+  (dbus:with-open-bus (bus (dbus:session-server-addresses))
+    (dbus:with-introspected-object
+        (notifications bus "/org/freedesktop/Notifications"
+                       "org.freedesktop.Notifications")
+      (notifications "org.freedesktop.Notifications" "Notify"
+                     "Test" 0 "" title content '() '() expire-timeout))))
+;; (notify-send "A" "B" :expire-timeout (-1))
+;; (notify-send "A" "B" :expire-timeout 2000)
