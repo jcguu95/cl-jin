9ecaf38adb80ce2875e020f90e1a362802ae143f
jcguu95 <jcguu95@gmail.com>
2021-07-25 14:14:01 -0500

    Re-new dmenu! Default to rofi.

--- utils/dmenu.lisp
+++ utils/dmenu.lisp
@@ -1,33 +1,25 @@
 (in-package :jin.utils)
 
-(defun dmenu-deprecated (candidates &optional prompt)
+(defun dmenu (candidates &optional prompt)
+  "Receive a list CANDIDATES of strings with a optional PROMPT.
+Let the user select a string using rofi or dmenu, and return the
+selected string."
+  (unless prompt (setf prompt ""))
   (string-right-trim
    '(#\Newline)
    (with-output-to-string (out)
      (with-input-from-string
          (in (format nil "~{~a~%~}" candidates))
-       (let ((font "terminus")
-             (size "18"))
-         (sb-ext:run-program (whereis "dmenu")
-                             (list "-p" prompt "-fn"
-                                   (concatenate 'string
-                                                font "-" size))
-                             :input in :output out))))))
-
-(defun dmenu (candidates &optional prompt)
-  (unless prompt (setf prompt ""))
-  (let* ((command (whereis "rofi"))
-         (service (make-instance
-                   'jin.service:service
-                   :name "rofi"
-                   :action `(lambda ()
-                              (with-output-to-string (out)
-                                (with-input-from-string
-                                    (in ,(format nil "~{~a~%~}" candidates))
-                                  (sb-ext:run-program
-                                   ,command
-                                   ',(list "-dmenu" "-p" prompt)
-                                   :input in
-                                   :output out
-                                   :error *standard-output*)))))))
-    (jin.service:dispatch service)))
+       (let ((rofi (whereis "rofi"))
+             (dmenu (whereis "dmenu"))
+             command args)
+         (if rofi
+             (progn (setf command rofi)
+                    (setf args (list "-dmenu" "-p" prompt)))
+             (progn (setf command dmenu)
+                    (setf args (list "-p" prompt "-fn"
+                                     (format nil "~a-~a" font size)))))
+         (sb-ext:run-program command args
+                             :input in
+                             :output out
+                             :error *standard-output*))))))
