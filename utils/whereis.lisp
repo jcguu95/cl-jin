0343392f9683dd84de0102bc7b508eb7e92d517e
jcguu95 <jcguu95@gmail.com>
2021-07-25 09:17:14 -0500

    fix bug: whereis

--- utils/whereis.lisp
+++ utils/whereis.lisp
@@ -1,10 +1,13 @@
 (in-package :jin.utils)
 
 (defun whereis (target)
-  (string-right-trim
-   (format nil "~%")
-   (with-output-to-string (out)
-     (uiop:run-program
-      (format nil "whereis ~a | cut -d' ' -f2" target)
-      ;; TODO make this return multiple values
-      :output out))))
+  (let ((result (string-right-trim
+                 (format nil "~%")
+                 (with-output-to-string (out)
+                   (uiop:run-program
+                    (format nil "whereis ~a | cut -d' ' -f2" target)
+                    ;; TODO make this return multiple values
+                    :output out)))))
+    ;; if exists it must start with the character #\/
+    (unless (eq #\/ (char result 0)) (setf result nil))
+    result))
